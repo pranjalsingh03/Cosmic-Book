d211f03a7713afe938c4b5236ddd8fedc8b184a8
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-20 19:27:54 +0530

    update: routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { WriteComponent } from './pages/write/write.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { ReadComponent } from './pages/read/read.component';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'write', component: WriteComponent },
+  { path: 'read/:id', component: ReadComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    WriteComponent,
+    HeaderComponent,
+    ReadComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
