3f4122476dbc59dc210b0628d62296d3c835e172
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-20 19:27:36 +0530

    Add read component with template, styles, and spec file

---
+++ src/app/pages/read/read.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-read',
+  templateUrl: './read.component.html',
+  styleUrls: ['./read.component.css']
+})
+export class ReadComponent implements OnInit {
+  books: any[] = [];
+  selectedBook: any;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+      if (id !== null) {
+        this.loadBookDetails(id);
+      }
+    });
+  }
+
+  loadBookDetails(id: string) {
+    this.http.get<any>('http://localhost:4200/assets/book.json').subscribe({
+      next: (data) => {
+        console.log('Fetched data:', data);
+        if (Array.isArray(data.books)) {
+          const book = data.books.find((item: any) => item.id === Number(id));
+          if (book) {
+            this.selectedBook = book;
+            console.log('Selected Book:', this.selectedBook);
+          } else {
+            console.error('Book not found with ID:', id);
+          }
+        } else {
+          console.error('Invalid data format. Expected an array.');
+        }
+      },
+      error: (err) => {
+        console.error('Error loading books:', err);
+      }
+    });
+  }
+}
