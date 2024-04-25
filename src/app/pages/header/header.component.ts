6037150b997ac295895f925490a929f3b2d83348
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-04-26 00:34:52 +0530

    Update header component styling and add search functionality

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(private http: HttpClient, private router: Router) { }
+  
+  ngOnInit() {
+    this.loadBooks();
+  }
+  
+  books: any[] = [];
+  searchTerm = "";
+  searchResults: any[] = [];
+  isSearchPopupOpen = false;
 
+  loadBooks() {
+    this.http.get<any>('https://cosmic-book.vercel.app/assets/book.json').subscribe({
+      next: (data) => {
+        if (Array.isArray(data.books)) {
+          this.books = data.books;
+        } else {
+          console.error('Invalid data format. Expected an array.');
+        }
+      },
+      error: (err) => {
+        console.error('Error loading books:', err);
+      }
+    });
+  }
+
+  onSearch() {
+    if (this.searchTerm.trim() !== "") {
+      this.searchResults = this.books.filter((book) => {
+        return book.title.toLowerCase().includes(this.searchTerm.toLowerCase());
+      });
+      this.isSearchPopupOpen = true;
+    } else {
+      this.searchResults = [];
+      this.isSearchPopupOpen = false;
+    }
+  }
+
+  closeSearchPopup() {
+    this.isSearchPopupOpen = false;
+  }
+  goToDetailsPage(id: string) {
+    this.router.navigate(['/read', id]);
+  }  
 }
